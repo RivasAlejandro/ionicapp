df83c8a7e83667941da375f03cb4b5739040f765
RivasAlejandro <joalrivashe@ittepic.edu.mx>
2020-08-16 17:06:59 -0600

    love me bitch

--- myfirstApp/src/app/app.module.ts
+++ myfirstApp/src/app/app.module.ts
@@ -8,12 +8,27 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import {ValidatorService} from '../app/validator.service';
+import { RouterModule } from "@angular/router";
+import { HomePage } from './home/home.page';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [ 
+    
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+     IonicModule.forRoot(), 
+    AppRoutingModule, 
+    BrowserAnimationsModule,
+  
+  
+  ],
   providers: [
+    ValidatorService,
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
--- myfirstApp/src/app/home/home.page.ts
+++ myfirstApp/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  form: FormGroup;
+  constructor(public formBuilder: FormBuilder) {
+    this.form = formBuilder.group({
+      userNameControl: [
+        "",
+        Validators.compose([
+          Validators.minLength(4),
+          Validators.maxLength(30),
+          Validators.pattern("[0-9a-z-A-Z-_]*"),
+          Validators.required
+        ])
+      ],
+      emailControl: [
+        "",
+        Validators.compose([
+          Validators.minLength(4),
+          Validators.pattern("[0-9a-z-A-Z@.]*"),
+          Validators.required
+        ])
+      ],
+      passwordControl: [
+        "",
+        Validators.compose([
+          Validators.minLength(8),
+          Validators.pattern("[0-9a-z-A-Z@.#*$!?&+-/]*"),
+          Validators.required
+        ])
+      ]
+    });
+    
 
-  constructor() {}
+  }
+  saveDetails(value) {
+    console.dir(value);
+  }
 
-}
+  }
+  
